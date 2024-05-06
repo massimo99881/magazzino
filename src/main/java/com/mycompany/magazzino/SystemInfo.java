d61649f673b3814e95479ccb9b353eff90ea8554
massimo99881 <massimo99881@gmail.com>
2024-05-06 18:39:14 +0200

    primo commit

---
+++ src/main/java/com/mycompany/magazzino/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.mycompany.magazzino;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
